6e3f22d4999b7d2d1748fc2bfa05007353bc4ca9
Chase-san <chasesan@gmail.com>
2013-05-22 04:44:30 -0400

    rewritten to be as singular as possible (tm)

--- 1-minimal/index.php
+++ 1-minimal/index.php
@@ -1,44 +1,36 @@
 <?php
 
-/**
- * A simple, clean and secure PHP Login Script
- * 
- * MINIMAL VERSION
- * (check the website / github / facebook for other versions)
- * 
- * A simple PHP Login Script without all the nerd bullshit.
- * Uses PHP SESSIONS, modern SHA512-password-hashing and salting
- * and gives the basic functions a proper login system needs.
- * 
- * Please remember: this is just the minimal version of the login script, so if you need a more
- * advanced version, have a look on the github repo. there are / will be better versions, including
- * more functions and/or much more complex code / file structure. buzzwords: MVC, dependency injected,
- * one shared database connection, PDO, prepared statements, PSR-0/1/2 and documented in phpDocumentor style
- * 
- * @package php-login
- * @author Panique <panique@web.de>
- * @link https://github.com/panique/php-login/
- * @license http://opensource.org/licenses/MIT MIT License
- */
+require 'account.php';
 
-// include the configs / constants for the database connection
-require_once('db_config.php');
+$account = new Account();
 
-// load the login class
-require_once('db_login.php');
+if($account->isLoggingIn()) {
+	//prints the return status of the login
+	print_r($account->login());
+}
 
-// create a login object. when this object is created, it will do all login/logout stuff automatically
-// so this single line handles the entire login process. in consequence, you can simply ...
-$login = new Login();
+if($account->isRegistering()) {
+	//prints the return status of the register
+	print_r($account->register());
+}
 
-// ... ask if we are logged in here:
-if ($login->isUserLoggedIn() == true) {    
-    // the user is logged in. you can do whatever you want here.
-    // for demonstration purposes, we simply show the "you are logged in" view.
-    include("views/logged_in.php");
-    
+if($account->isLoggedIn()) {
+	echo '<h3>Welcome '.$account->getUsername().'</h3>';
+	echo '<a href="index.php?logout">Logout</a>';
 } else {
-    // the user is not logged in. you can do whatever you want here.
-    // for demonstration purposes, we simply show the "you are not logged in" view.
-    include("views/not_logged_in.php");
+	?>
+	<h3>Login</h3>
+	<form method="post" action="index.php">
+		<input type="text" placeholder="username" name="username"><br>
+		<input type="password" placeholder="password" name="password"><br><br>
+		<input type="submit" name="login" value="Log In">
+	</form>
+	<h3>Register</h3>
+	<form method="post" action="index.php">
+		<input type="text" placeholder="username" name="username"><br>
+		<input type="password" placeholder="password" name="password"><br>
+		<input type="password" placeholder="password again" name="password_repeat"><br><br>
+		<input type="submit" name="register" value="Register">
+	</form>
+	<?php
 }
